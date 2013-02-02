8977294797d543839096ee20f01a6cb9ed377c72
ankurpandit <ankurpanditce@gmail.com>
2013-02-03 00:36:36 +0530

    colts

--- app/Controller/ColtsController.php
+++ app/Controller/ColtsController.php
@@ -3,15 +3,19 @@ class ColtsController extends AppController {
 
 	public $name = 'Colts';
 	public $helpers = array('Html','Form');	
-	public $uses = array('Club');
+	public $uses = array('Club','Colt');
 	
 	public function beforeFilter(){
 		$this->layout = 'club';
 		$this -> set('club_info', $this->Club->find('first' ,array('conditions' => 
-														array('Club.website' => $this->request->params['club']))));	
+														array('Club.website' => $this->request->params['club']))));
+		$this->clubId = $this->getClubId();	
 	}
 	
 	public function index() {
+		$id = $this->clubId['Club']['id'];
+		$this-> set('coltInfo', $this->Colt->find('all' ,array('conditions' => 
+							array('Colt.club_id' => $id))));
 	}	
 }
 ?>
